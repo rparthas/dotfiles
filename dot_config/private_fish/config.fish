fbff02ba7e81a14358698839cf890f53f8a177c8
Rajagopal <rajagopal.parthasarathi@hingehealth.com>
2026-02-20 10:19:08 +0530

    Modified config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,10 +2,3 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 export PATH="$HOME/.local/bin:$PATH"
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
-
-# Added by Antigravity
-fish_add_path /Users/rajagopal.parthasarathi/.antigravity/antigravity/bin
